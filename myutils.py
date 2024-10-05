e407c2591ab420602db8bb15445c0e910f229b3c
slovanos <apolos@gmail.com>
2024-10-05 10:38:59 +0200

    add get_new_file_path in myutils
    Generates a new file path with an optional suffix and/or extension

--- myutils.py
+++ myutils.py
@@ -137,6 +137,26 @@ def list_files(path, extensions=''):
     return sorted(files_list)
 
 
+def get_new_file_path(
+    file_path: str, suffix: str = '_modified', extension: str | None = None
+) -> str:
+    """
+    Generates a new file path with an optional suffix and/or extension.
+
+    Args:
+        file_path (str): The original file path.
+        suffix (str, optional): The suffix to add to the file name. Defaults to '_modified'.
+        extension (str | None, optional): The desired new file extension. If not provided, it will use the original extension.
+
+    Returns:
+        str: The new file path with the specified suffix and/or extension.
+    """
+    dir_name, file_base = os.path.split(file_path)
+    file_name, orig_ext = os.path.splitext(file_base)
+    ext = extension if extension else orig_ext
+    return os.path.join(dir_name, file_name + suffix + ext)
+
+
 def download_file(url, file_name=None):
     """Downloads file from url and stores it as 'file_name' if given"""
     try:
